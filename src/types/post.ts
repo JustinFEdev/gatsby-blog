58b51a14a7ca240046c523673c9d504c129e7ce0
Justin_supertree <justin1688@supertree.co>
2022-01-24 17:15:13 +0900

    1/24 basic layout 설정

---
+++ src/types/post.ts
@@ -0,0 +1,12 @@
+export type groupType = {
+    tag?: string;
+    totalCount?: number;
+};
+
+export type postType = {
+    title: string;
+    data: string;
+    slug: string;
+    tags: string[];
+    path: string;
+};
