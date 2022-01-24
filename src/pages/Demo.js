ded4851afafaeccf015b8300a6aee1fbf550bf2a
Justin_supertree <justin1688@supertree.co>
2022-01-24 16:04:47 +0900

    test commit

---
+++ src/pages/Demo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Demo = () => {
+    return <div>Here is Demo Page</div>;
+};
+
+export default Demo;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,8 @@ const IndexPage: React.FC = () => (
             <Link to="/page-2/">Go to page 2</Link> <br />
             <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
             <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-            <Link to="/using-dsg">Go to "Using DSG"</Link>
+            <Link to="/using-dsg">Go to "Using DSG"</Link> <br />
+            <Link to="/Demo">Demo page</Link>
         </p>
     </Layout>
 );
