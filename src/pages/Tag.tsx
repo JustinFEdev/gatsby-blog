6fed470e4585a9fcc66510c90c285a645a1b4467
Justin_supertree <justin1688@supertree.co>
2022-02-03 17:36:43 +0900

    test

--- src/pages/Tag.tsx
+++ src/pages/Tag.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Layout from '../components/layout/Layout';
+import Seo from '../components/common/seo';
 
 const Tag = () => {
-    return <div>Tag Page</div>;
+    return (
+        <Layout>
+            <Seo title="Page Tag" />
+            Tag Page
+        </Layout>
+    );
 };
 
 export default Tag;
