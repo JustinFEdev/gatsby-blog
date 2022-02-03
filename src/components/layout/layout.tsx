0141c3187b74811e07d44c1d323191c2a81c6238
Justin_supertree <justin1688@supertree.co>
2022-02-03 18:02:40 +0900

    remove scoll Y layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
-
+import { Link } from 'gatsby';
 import Header from './Header';
-// import './layout.css';
+import './layout.css';
 
 const Layout: React.FC = ({ children }) => {
     const data = useStaticQuery(graphql`
@@ -19,6 +19,32 @@ const Layout: React.FC = ({ children }) => {
     return (
         <>
             <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+            <div style={{ textAlign: 'center' }}>
+                {/* <h1>Justin's Dev Log</h1> */}
+                <ul
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        color: '#000',
+                        outline: 'none',
+                        listStyleType: 'none',
+                    }}
+                >
+                    <li style={{ margin: '0 20px', textDecoration: 'none' }}>
+                        <Link to="/Me/">Justin</Link>
+                    </li>
+                    <li style={{ margin: '0 20px' }}>
+                        <Link to="/using-typescript/">Post</Link>
+                    </li>
+                    <li style={{ margin: '0 20px' }}>
+                        <Link to="/Tag/">Tags</Link> <br />
+                    </li>
+                    <li style={{ margin: '0 20px' }}>
+                        <Link to="/using-dsg">Else</Link> <br />
+                    </li>
+                </ul>
+            </div>
             <div
                 style={{
                     margin: `0 auto`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import { css } from '@emotion/react';
 
@@ -15,32 +14,7 @@ const IndexPage = () => {
     return (
         <Layout>
             <Seo title="Home" />
-            <div style={{ textAlign: 'center' }}>
-                {/* <h1>Justin's Dev Log</h1> */}
-                <ul
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        color: '#000',
-                        outline: 'none',
-                        listStyleType: 'none',
-                    }}
-                >
-                    <li style={{ margin: '0 20px', textDecoration: 'none' }}>
-                        <Link to="/Me/">Justin</Link>
-                    </li>
-                    <li style={{ margin: '0 20px' }}>
-                        <Link to="/using-typescript/">Post</Link>
-                    </li>
-                    {/* <li style={{ margin: '0 20px' }}>
-                        <Link to="/Tag">Tags</Link> <br />
-                    </li> */}
-                    <li style={{ margin: '0 20px' }}>
-                        <Link to="/using-dsg">Else</Link> <br />
-                    </li>
-                </ul>
-            </div>
+
             <div style={{ height: '100vh', border: '1px solid', padding: 30 }}>
                 <h1 css={topMenu}>Who Am I?</h1>
                 <StaticImage
