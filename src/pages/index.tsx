05fcfdaddaeb7b9186e3d98787299fa34357115e
Justin_supertree <justin1688@supertree.co>
2022-02-04 10:28:22 +0900

    2/4 update home page,add image file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
 import { css } from '@emotion/react';
 
 import Layout from '../components/layout/Layout';
 import Seo from '../components/common/seo';
-
-const topMenu = css`
-    color: #000;
-    font-size: 50px;
-`;
 
 const IndexPage = () => {
     return (
